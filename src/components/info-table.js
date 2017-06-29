0f1fc9333d65a1eefedf9c07b4bec65a51f492cd
Trevor Pellegrini <trevorpellegrini@gmail.com>
2017-06-29 11:11:46 -0700

    MVP

---
+++ src/components/info-table.js
@@ -0,0 +1,73 @@
+'use strict';
+import React from 'react';
+
+const InfoTable = props => {
+    if (props.data.show) {
+        return (
+            <div className='floatLeft'>
+                <div className='description-popup' onClick={props.data.showDescription}>
+                    <div className='description-title'>Description</div>
+                    <hr/>
+                <div className='description-body'>{props.data.place.description}</div>
+                </div>
+                <div className='temp'>
+                    <h2>{String(props.data.weather.locationData.currently.temperature).split('.')[0] + '°F'}</h2>
+                </div>
+                <table>
+                    <tbody>
+                        <tr>
+                            <th>City</th>
+                        <td>{props.data.place.city}</td>
+                        </tr>
+                        <tr>
+                            <th>Name</th>
+                        <td>{props.data.place.name}</td>
+                        </tr>
+                        <tr>
+                            <th>Description</th>
+                            <td>
+                                <button className='button is-inf' onClick={props.data.showDescription}>Read</button>
+                            </td>
+                        </tr>
+                        <tr>
+                            <th>Activities</th>
+                        <td>{props.data.activities.join(', ')}</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        )
+    } else {
+        return (
+            <div className='floatLeft'>
+                <div className='temp'>
+                    <h2>{String(props.data.weather.locationData.currently.temperature).split('.')[0] + '°F'}</h2>
+                </div>
+                <table>
+                    <tbody>
+                        <tr>
+                            <th>City</th>
+                        <td>{props.data.place.city}</td>
+                        </tr>
+                        <tr>
+                            <th>Name</th>
+                        <td>{props.data.place.name}</td>
+                        </tr>
+                        <tr>
+                            <th>Description</th>
+                            <td>
+                                <button className='button is-inf' onClick={props.data.showDescription}>Read</button>
+                            </td>
+                        </tr>
+                        <tr>
+                            <th>Activities</th>
+                        <td>{props.data.activities.join(', ')}</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        )
+    }
+}
+
+export default InfoTable;
